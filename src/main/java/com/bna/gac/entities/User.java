c79e4f69b7152d3ffe9039c009516c4ac9fab80a
gac <aaskri001@gmail.com>
2026-02-25 12:03:29 +0100

    first commit

--- src/main/java/com/bna/gac/entities/User.java
+++ src/main/java/com/bna/gac/entities/User.java
@@ -3,6 +3,9 @@ package com.bna.gac.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "users")
 @Getter
@@ -16,9 +19,20 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, nullable = false)
     private String username;
 
+    @Column(unique = true, nullable = false)
     private String email;
 
     private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    @Builder.Default
+    private Set<Role> roles = new HashSet<>();
 }
--- src/main/java/com/bna/gac/security/JwtService.java
+++
@@ -1,4 +0,0 @@
-package com.bna.gac.security;
-
-public class JwtService {
-}
